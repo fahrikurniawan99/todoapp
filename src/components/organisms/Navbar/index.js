f1d4d9626010fd52e965e675cca61f6599a90ecb
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-26 15:21:33 +0700

    percobaan ke 2

--- src/components/organisms/Navbar/index.js
+++ src/components/organisms/Navbar/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import * as Icon from '../../../assets/images/icon'
 
 function index() {
   return (
       <div className="flex justify-between items-center px-1 py-2 shadow-md">
         <h1 className='text-2xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
-        <Icon.menu/>
       </div>
   )
 }
