040536478fc11c4ffd10b6d1d67e9644f06ef9d7
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-26 15:06:02 +0700

    first commit dan mencoba ui navbar

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import "../assets/css/app/App.css";
+import Router from "../routes";
+import Home from "./Home";
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../../components/organisms'
+
+function index() {
+  return (
+    <div className='container mx-auto p-1'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default index
