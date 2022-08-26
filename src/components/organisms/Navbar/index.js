ed7e743c147804da20c313eb1bfd8c2d5d28541b
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-26 15:33:15 +0700

    mengganti ukuran font pada brand navbar

--- src/components/organisms/Navbar/index.js
+++ src/components/organisms/Navbar/index.js
@@ -4,7 +4,7 @@ import Menu from '../../../assets/images/svg/Menu'
 function index() {
   return (
       <div className="flex justify-between items-center px-1 py-2 shadow-md">
-        <h1 className='text-3xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
+        <h1 className='text-2xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
         <Menu/>
       </div>
   )
