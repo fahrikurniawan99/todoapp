0e2cc178750ff8df2128182f6f32bbdbf69fa137
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-26 15:28:45 +0700

    menambah icon menu pada navbar

--- src/components/organisms/Navbar/index.js
+++ src/components/organisms/Navbar/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Menu from '../../../assets/images/svg/Menu'
 
 function index() {
   return (
       <div className="flex justify-between items-center px-1 py-2 shadow-md">
-        <h1 className='text-2xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
+        <h1 className='text-3xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
+        <Menu/>
       </div>
   )
 }
