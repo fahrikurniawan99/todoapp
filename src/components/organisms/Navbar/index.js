0bef313ff2309a3dbd3be9e646df0fa1ddfc075b
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-26 16:37:43 +0700

    padding navbar

--- src/components/organisms/Navbar/index.js
+++ src/components/organisms/Navbar/index.js
@@ -3,7 +3,7 @@ import Menu from '../../../assets/images/svg/Menu'
 
 function index() {
   return (
-      <div className="flex justify-between items-center px-1 py-2 shadow-md">
+      <div className="flex justify-between items-center px-1 py-3 shadow-md">
         <h1 className='text-2xl font-semibold text-indigo-500 cursor-pointer'>TodoApp</h1>
         <Menu/>
       </div>
