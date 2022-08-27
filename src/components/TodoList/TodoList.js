22ba0c32134cf350ba1625609a391dd496eede4c
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-27 10:49:33 +0700

    install headless ui

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,19 +4,19 @@ function TodoList(props) {
   const todos = props.todos;
   return (
     <div className="mt-10">
-      <div className="bg-gray-100 w-full shadow-lg rounded-sm px-3 pb-9 pt-5 relative space-y-5">
-        <h1 className="absolute bg-gray-100 -top-3 left-0 font-bold text-gray-600 px-4 py-1 rounded-lg">
+      <div className="bg-indigo-400 w-full shadow-xl rounded-md px-3 pb-9 pt-5 relative space-y-5">
+        <h1 className="absolute bg-indigo-400 -top-3 left-0 font-bold text-white px-4 py-1 rounded-lg">
           Todo List
         </h1>
         {!todos.length ? (
-          <h1 className="text-base font-semi-bol text-gray-600 text-center ">
-            Not Todo <span className="animate-pulse font-bold">...</span>
+          <h1 className="text-lg font-medium animate-pulse text-indigo-600 text-center ">
+            Not Todo ...
           </h1>
         ) : (
           todos.map((todo) => {
             return (
               <div
-              className="bg-white w-full px-4 text-md py-3 flex justify-between items-center shadow-md rounded-md"
+              className="bg-gray-100 w-full px-4 text-md py-3 flex justify-between items-center shadow-lg rounded-md"
               key={todo.id}
               >
                 <input type="checkbox" name="" id="" />
